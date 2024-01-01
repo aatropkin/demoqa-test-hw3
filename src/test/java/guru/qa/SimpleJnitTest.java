2366aad23d6a18a3dd999503d979d8ecda371185
Alex <aatropkin@yandex.ru>
2024-01-01 23:49:01 +0300

    added junit5

---
+++ src/test/java/guru/qa/SimpleJnitTest.java
@@ -0,0 +1,38 @@
+package guru.qa;
+
+import org.junit.jupiter.api.*;
+
+public class SimpleJnitTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("### @BeforeAll!!!!!!!!!!!!!!!!!!!!!");
+    }
+    @AfterAll
+    static void afterAll(){
+        System.out.println("### @AfterAll!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+    @BeforeEach
+    void beforEach(){
+        System.out.println("### @BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("### @AfterEach");
+    }
+
+
+    @Test
+    void firstTest() {
+        System.out.println("### @Test firstTest");
+        Assertions.assertTrue(2 * 2 == 4);
+    }
+    @Test
+    void secondTest() {
+        System.out.println("### @Test secondTest");
+        Assertions.assertTrue(2 + 2 == 4);
+    }
+
+}
